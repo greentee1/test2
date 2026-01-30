02157cf0193893d63a537f7c715055691af11cbd
Klara <stydy000coffee@gmail.com>
2026-01-31 00:37:44 +0300

    feat: добавление математических преобразований

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,22 @@
 import binascii
 
 class SAFEREncrypt:
+        def __init__(self, rounds=8):
+        self.rounds = min(max(rounds, 6), 10)
+        self.logtab = [0] * 256
+        self.exptab = [0] * 257
+        self._init_tables()
+
+    def _init_tables(self):
+        self.exptab[0] = 1
+        for i in range(1, 256):
+            val = (45 * self.exptab[i - 1]) % 257
+            if val == 256:
+                val = 0
+            self.exptab[i] = val
+            self.logtab[val] = i
+
+        self.logtab[1] = 0
+        self.logtab[0] = 128
+        self.exptab[128] = 0
+        self.exptab[256] = 0
