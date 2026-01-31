d2eb07883368fb27c56b313ef226dabeac676345
Klara <stydy000coffee@gmail.com>
2026-01-31 05:49:39 +0300

    feat: Добавления класса дешифрования

--- decrypt.py
+++ decrypt.py
@@ -1,1 +1,9 @@
 import binascii
+
+class SAFERDecrypt:
+    
+    def __init__(self, rounds=8):
+        self.rounds = min(max(rounds, 6), 10)
+        self.logtab = [0] * 256
+        self.exptab = [0] * 257
+        self._init_tables()
