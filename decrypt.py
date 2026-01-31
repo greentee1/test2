6bd7956351d27aff879751436ca34361a2c38ddd
Klara <stydy000coffee@gmail.com>
2026-01-31 05:52:15 +0300

    feat: инициализация таблиц exp/log

--- decrypt.py
+++ decrypt.py
@@ -7,3 +7,22 @@ class SAFERDecrypt:
         self.logtab = [0] * 256
         self.exptab = [0] * 257
         self._init_tables()
+
+
+    def _init_tables(self):
+
+        self.exptab[0] = 1
+        for i in range(1, 256):
+            val = (45 * self.exptab[i - 1]) % 257
+            if val == 256:
+                val = 0
+            self.exptab[i] = val
+            self.logtab[val] = i
+
+        self.logtab[1] = 0
+        self.logtab[0] = 128
+        self.exptab[128] = 0
+        self.exptab[256] = 0
+
+    def rotate_left(self, b, n):
+        return ((b << n) | (b >> (8 - n))) & 0xFF
